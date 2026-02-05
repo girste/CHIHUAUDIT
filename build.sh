1614214f1ed987ef8cb286b570c3fb32766035ba
girste <me@girste.com>
2026-02-05 13:52:12 +0000

    Update Go to latest stable version

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo "Building chihuaudit with Docker..."
 docker run --rm \
   -v "$PWD":/app \
   -w /app \
-  golang:1.21-alpine \
+  golang:alpine \
   sh -c "CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags='-s -w' -o bin/chihuaudit"
 
 echo "Build complete: bin/chihuaudit"
